1bb44ec7efd74d7a5aad6749eaaaa041f0af5ef5
BParadowski <bartoszparadowski01@gmail.com>
2022-07-25 18:48:43 +0200

    Add basic functionality, render lib and remove books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const bookshelf = document.querySelector('.bookshelf');
 
 function Book(title, author, pages, isRead){
     this.title = title;
@@ -16,21 +17,76 @@ function addBookToLibrary(title, author, pages, isRead){
     myLibrary.push(new Book(title, author, pages, isRead));
 }
 
-function displayLibrary(){
-    for (let book of myLibrary){
-        let newBook = document.createElement('div');
+function displayBook(i){
+
+    if (myLibrary[i] !== undefined){
+
+    const book = myLibrary[i];
+    let newBook = document.createElement('div');
         newBook.classList.add("book");
+//book elements
         let newTitle = document.createElement('h4')
         newTitle.classList.add('book__title');
         newTitle.textContent = book.title;
         let newAuthor = document.createElement('p');
         newAuthor.classList.add('book__author');
         newAuthor.textContent = `by ${book.author}`;
-        let 
+        let newPages = document.createElement('p');
+        newPages.classList.add('book__pages');
+        newPages.textContent = `Pages: ${book.pages}`;
+        let buttons = document.createElement('div');
+        buttons.classList.add('book__buttons');
+// buttons
+        let newlabel = document.createElement('label');
+        newlabel.classList.add('book__switch');
+        let newCheckbox = document.createElement('input');
+        newCheckbox.type = 'checkbox';
+        newCheckbox.checked = book.isRead;
+        let newSlider = document.createElement('span');
+        newSlider.classList.add('book__slider');
 
+        let newButton = document.createElement('button');
+        newButton.classList.add('book__delete');
+// putting it together
+
+        newlabel.appendChild(newCheckbox);
+        newlabel.appendChild(newSlider);
+
+        buttons.appendChild(newlabel);
+        buttons.appendChild(newButton);
+        newButton.dataset.id = `${i}`;
+        newButton.addEventListener('click', removeBook)
+
+        newBook.appendChild(newTitle);
+        newBook.appendChild(newAuthor);
+        newBook.appendChild(newPages);
+        newBook.appendChild(buttons);
+
+        bookshelf.appendChild(newBook);
+        newBook.id = `book_${i}`;
     }
+}
+
+function displayNewBook(){
+    displayBook(myLibrary.length-1);
+}
+
+function displayLibrary(){
+    for (let i = 0; i < myLibrary.length; i++){
+        displayBook(i);
+    }
+}
+
+function removeBook(e){
+    let bookId = `book_${this.dataset.id}`;
+    myLibrary[this.dataset.id] = undefined;
+    let bookToDelete = document.getElementById(bookId);
+    bookToDelete.replaceChildren();
+    bookToDelete.remove()
 }
 
 addBookToLibrary("The hobbit", "J R R Tolkien", 255, true);
 addBookToLibrary("The hobbit", "J R R Tolkien", 255, true);
 addBookToLibrary("The hobbit", "J R R Tolkien", 255, true);
+
+displayLibrary();
